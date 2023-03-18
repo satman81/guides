a4b8869206c621df9e45d03f77365d873022050e
SATMan <101592073+satman81@users.noreply.github.com>
2023-03-18 11:17:29 +0100

    Update caddy.sh

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -1,3 +1,5 @@
+#https://github.com/satman81/guides/blob/main/caddy/readme.md
+
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
