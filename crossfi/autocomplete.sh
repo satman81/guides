2f6a65d216e332b595e9de5ebc72a1d8ad999a5e
SATMan <101592073+satman81@users.noreply.github.com>
2024-02-25 09:08:36 +0100

    Update autocomplete.sh

--- crossfi/autocomplete.sh
+++ crossfi/autocomplete.sh
@@ -1,19 +1,21 @@
-# by SATMan - https://github.com/satman81/                                        
+# by SATMan - https://github.com/satman81
 # v0.1 - 02.2024
-_generate_crossfid_completions() {
-    local cur prev opts cmds
+
+# Define the binary
+base_cmd="crossfid"
+
+_generate_command_completions() {
+    local cur prev opts cmds flags
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    # Define the base command
-    local base_cmd="crossfid"
-
-    # Initialize commands string
+    # Initialize commands and flags strings
     cmds=""
+    flags=""
 
     # Fetch the top-level commands if no command has been entered yet
     if [[ ${COMP_CWORD} -eq 1 ]]; then
-        cmds=$(crossfid --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
+        cmds=$($base_cmd --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
     fi
 
     # Determine the active command or sub-command
@@ -24,33 +26,34 @@ _generate_crossfid_completions() {
             if [[ -z "$command" ]]; then
                 command="${COMP_WORDS[i]}"
                 # Fetch sub-commands for the detected command
-                cmds=$(crossfid $command --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
+                cmds=$($base_cmd $command --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
             else
                 subcommand="${COMP_WORDS[i]}"
                 # Fetch sub-command options if a sub-command is detected
-                cmds=$(crossfid $command $subcommand --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
+                cmds=$($base_cmd $command $subcommand --help 2>/dev/null | awk '/Commands:/,/^$/' | grep -vE 'Commands:|Available Commands:' | awk '{print $1}' | sort -u)
                 break
             fi
         fi
     done
-
-    # Fetch and parse options based on the current context
-    opts=""
+   
+    # Fetch flags for the current context
     if [[ -n "$subcommand" ]]; then
-        opts+=" $(crossfid $command $subcommand --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
+        flags+=" $($base_cmd $command $subcommand --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
     elif [[ -n "$command" ]]; then
-        opts+=" $(crossfid $command --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
+        flags+=" $($base_cmd $command --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
     else
-        opts+=" $(crossfid --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
+        flags+=" $($base_cmd --help 2>/dev/null | grep -oE '\-\-[a-zA-Z0-9\-]+' | sort -u)"
     fi
 
-    # Combine commands and options for completion
+    # Combine commands, subcommands, and flags for completion, prioritizing commands and subcommands
     COMPREPLY=()
     if [[ "$cur" == --* ]]; then
-        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+        COMPREPLY=($(compgen -W "${flags}" -- "${cur}"))
     else
-        COMPREPLY=($(compgen -W "${cmds} ${opts}" -- "${cur}"))
+        COMPREPLY=($(compgen -W "${cmds}" -- "${cur}"))
+        # If no commands match, then suggest flags
+        [[ ${#COMPREPLY[@]} -eq 0 ]] && COMPREPLY=($(compgen -W "${flags}" -- "${cur}"))
     fi
 }
 
-complete -F _generate_crossfid_completions crossfid
+complete -F _generate_command_completions $base_cmd 
